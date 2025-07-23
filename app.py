ce2685fe33aeea1e299b1ad5274ddc8f014f7b52
Ritzsigh <rituraj.singh@accredian.com>
2025-07-24 01:22:23 +0530

    Initial commit

--- app.py
+++ app.py
@@ -11,3 +11,7 @@ def webhook():
     data = request.get_json()
     print("Received data:", data)
     return jsonify({"status": "success", "received": data}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
+
